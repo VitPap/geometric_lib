c984e76f4f9ab8f7722dc561be0faf5249430565
VitPap <vitalik_gomel@tut.by>
2023-10-02 08:44:50 +0300

    [docs]: adding function description

--- circle.py
+++ circle.py
@@ -1,10 +1,12 @@
 import math
 
 
-def area(r):
+def area(r): 
+    '''принимает радиус r(float), возвращает площадь окружности(float)'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''принимает радиус r(float), возвращает периметр окружности(float)'''
     return 2 * math.pi * r
 
